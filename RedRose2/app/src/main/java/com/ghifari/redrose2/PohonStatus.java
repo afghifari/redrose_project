c799542a4c4e85e99952ca182aa8586979c0b6a1
hasnank <hasnurk@gmail.com>
2017-03-01 14:07:01 +0700

    fixed water activity

--- RedRose2/app/src/main/java/com/ghifari/redrose2/PohonStatus.java
+++ RedRose2/app/src/main/java/com/ghifari/redrose2/PohonStatus.java
@@ -3,11 +3,13 @@ package com.ghifari.redrose2;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
@@ -103,6 +105,55 @@ public class PohonStatus extends AppCompatActivity{
         final Intent service = new Intent(getBaseContext(), NatureBackgroundService.class);
         startService(service);
     }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+
+        super.onConfigurationChanged(newConfig);
+        ImageView imageView;
+        imageView = (ImageView) findViewById(R.id.watering_can);
+
+        switch (newConfig.orientation) {
+            case Configuration.ORIENTATION_LANDSCAPE:
+                Log.v("masuk", "configurationchanged");
+                imageView.setImageResource(R.mipmap.watering_can);
+                myPohon.Siram();
+                t_nutrition.setText(myPohon.getNutrition()+"");
+                SavePohon();
+                break;
+
+            case Configuration.ORIENTATION_PORTRAIT:
+                imageView.setImageResource(0);
+                break;
+
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState (Bundle outState) {
+        Log.v("activ", "AKUUUUUUUU");
+        super.onSaveInstanceState(outState);
+        Log.v("activ", "AKUUUUUUUU2");
+        outState.putString("age",(String)t_age.getText());
+        outState.putString("height",(String)t_height.getText());
+        outState.putString("nutrition",(String)t_nutrition.getText());
+        outState.putString("type",(String)t_type.getText());
+        Log.v("activ", "AKUUUUUUUU3");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.v("activ", "AKUUUUUUUU4");
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.v("activ", "AKUUUUUUUU5");
+        Log.v("activ", savedInstanceState.getString("age"));
+        t_age.setText(savedInstanceState.getString("age"));
+        t_height.setText(savedInstanceState.getString("height"));
+        t_nutrition.setText(savedInstanceState.getString("nutrition"));
+        t_type.setText(savedInstanceState.getString("type"));
+        Log.v("activ", "AKUUUUUUUU6");
+    }
+
 
     private Pohon myPohon;
     TextView t_height;
